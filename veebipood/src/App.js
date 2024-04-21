e6609b5c077dcb91c0b7d681409d11fdac39bcc5
eiramada <113309511+eiramada@users.noreply.github.com>
2024-04-21 12:24:20 +0300

    react-router outlet

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,19 +1,19 @@
-import "./App.css"
 import { Navigate, Route, Routes } from "react-router-dom";
-import Avaleht from "./pages/Avaleht";
-import Kinkekaart from "./pages/Kinkekaart";
-import Esindused from "./pages/Esindused";
-import Ariklient from "./pages/Ariklient";
-import Ostukorv from "./pages/Ostukorv";
+// import { Outlet, createBrowserRouter } from "react-router-dom";
+import "./App.css";
 import Menyy from "./components/Menyy";
+import Ariklient from "./pages/Ariklient";
+import Avaleht from "./pages/Avaleht";
+import Esindused from "./pages/Esindused";
+import Kinkekaart from "./pages/Kinkekaart";
 import Lisatoode from "./pages/LisaToode";
-import Seaded from "./pages/Seaded";
 import NotFound from "./pages/NotFound";
+import Ostukorv from "./pages/Ostukorv";
+import Seaded from "./pages/Seaded";
 
 export function App() {
   return (
     <div className="App">
-      
       <Menyy />
 
       {/* path="", mis järgneb localhost:3000-le, ja <div>, millist sisu näidatakse*/}
@@ -28,7 +28,29 @@ export function App() {
         <Route path="seaded" element={<Seaded />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
+      {/* <div>
+        <Outlet />
+      </div> */}
     </div>
   );
 }
 export default App;
+
+// Sedasi peaks kaasaegne React-Router olema seadistatud vastavalt reactrouter.com soovitustele. See, mis seal varem oli, on ka toimiv, aga see ilmselt kaob mingi aja pärast ära.
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {path: "", element: <Avaleht />},
+//       {path: "avaleht", element: <Avaleht />},
+//       {path: "osta-kinkekaart", element: <Kinkekaart />},
+//       {path: "esindused", element: <Esindused />},
+//       {path: "ärikliendile", element: <Ariklient />},
+//       {path: "ostukorv", element: <Ostukorv />},
+//       {path: "lisa-toode", element: <Lisatoode />},
+//       {path: "seaded", element: <Seaded />},
+//       {path: "*", element: <NotFound />},
+//     ]
+//   }
+// ]);
--- veebipood/src/index.js
+++ veebipood/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client"; //node-module'ist
 import "./index.css"; //meie failist
 import App from "./App";
 import { BrowserRouter } from "react-router-dom"; //node_module'ist
+// import App, { router } from "./App"; //uuemeelsem router'damine
+// import { RouterProvider } from "react-router-dom";  //uuemeelsem router'damine
 import 'react-toastify/dist/ReactToastify.css';
 //kui on loogelised sulud, siis võtab tüki sellest moodulist
 //kui ei ole {}, siis võtab terve selle mooduli
@@ -13,5 +15,7 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+
+    {/* <RouterProvider router={router}/> */}
   </React.StrictMode>
 );

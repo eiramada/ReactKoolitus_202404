9480a9ee6aa8287a972bbd385ce8f9392fff84ef
eiramada <113309511+eiramada@users.noreply.github.com>
2024-04-21 23:13:53 +0300

    sorted index.js

--- veebipood/src/index.js
+++ veebipood/src/index.js
@@ -1,13 +1,13 @@
 import React from "react"; //node_module'ist
 import ReactDOM from "react-dom/client"; //node-module'ist
-import "./index.css"; //meie failist
-import App from "./App";
 import { BrowserRouter } from "react-router-dom"; //node_module'ist
-// import App, { router } from "./App"; //uuemeelsem router'damine
-// import { RouterProvider } from "react-router-dom";  //uuemeelsem router'damine
-import 'react-toastify/dist/ReactToastify.css';
 //kui on loogelised sulud, siis võtab tüki sellest moodulist
 //kui ei ole {}, siis võtab terve selle mooduli
+import App from "./App";
+import "./index.css"; //meie failist
+import 'react-toastify/dist/ReactToastify.css';
+// import App, { router } from "./App"; //uuemeelsem router'damine
+// import { RouterProvider } from "react-router-dom";  //uuemeelsem router'damine
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

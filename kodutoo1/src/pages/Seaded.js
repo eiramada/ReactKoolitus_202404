2f22a6406c5b028e247692b78fadc3a2ab51655f
eiramada <113309511+eiramada@users.noreply.github.com>
2024-04-21 23:31:27 +0300

    localStorage

--- kodutoo1/src/pages/Seaded.js
+++ kodutoo1/src/pages/Seaded.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Seaded() {
+  const [kujundus, muudaKujundust] = useState(
+    localStorage.getItem("veebilehe_kujundus")
+  );
+
+  function muudaTumedaks() {
+    localStorage.setItem("veebilehe_kujundus", "dark_mode");
+    muudaKujundust("dark_mode");
+  }
+
+  function muudaHeledaks() {
+    localStorage.setItem("veebilehe_kujundus", "light_mode");
+    muudaKujundust("light_mode");
+  }
+
   return (
-    <div>Seaded</div>
-  )
+    <div>
+      <button onClick={muudaTumedaks}>TumeVeeb</button>
+      <button onClick={muudaHeledaks}>HeleVeeb</button>
+
+      {kujundus === "dark_mode" && <div>TUME LEHT</div>}
+      {kujundus === "light_mode" && <div>HELE LEHT</div>}
+    </div>
+  );
 }
 
-export default Seaded
+export default Seaded;

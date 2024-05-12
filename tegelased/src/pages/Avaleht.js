1a84fbd08208e7714761fc30ab43bb62061c3324
eiramada <113309511+eiramada@users.noreply.github.com>
2024-05-12 21:37:53 +0300

    arrays -> objects

--- tegelased/src/pages/Avaleht.js
+++ tegelased/src/pages/Avaleht.js
@@ -1,33 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Avaleht() {
+  const [tegelane, setTegelane] = useState("");
+
+  const tegelased = [
+    {
+      firstName: "Mickey",
+      lastName: "Mouse",
+      location: "Disneyland",
+    },
+    {
+      firstName: "Minnie",
+      lastName: "Mouse",
+      location: "Disneyland",
+    },
+    {
+      firstName: "Winnie",
+      lastName: "Pooh",
+      location: "Hundred Acre Wood",
+    },
+    {
+      firstName: "Roo",
+      lastName: "Kangaroo",
+      location: "Hundred Acre Wood",
+    },
+    {
+      firstName: "Scooby",
+      lastName: "Doo",
+      location: "Crystal Cove",
+    },
+  ];
+
+  const kuvaNimi = (tegelane) => {
+    console.log(tegelane.firstName);
+    setTegelane(tegelane.firstName + " " + tegelane.lastName);
+  };
+
   return (
     <div>
-      <div>
-        <div>Mickey</div>
-        <div>Mouse</div>
-        <div>Disneyland</div>
-      </div>
-      <div>
-        <div>Minnie</div>
-        <div>Mouse</div>
-        <div>Disneyland</div>
-      </div>
-      <div>
-        <div>Winnie</div>
-        <div>Pooh</div>
-        <div>Hundred Acre Wood</div>
-      </div>
-      <div>
-        <div>Roo</div>
-        <div>Kangaroo</div>
-        <div>Hundred Acre Wood</div>
-      </div>
-      <div>
-        <div>Scooby</div>
-        <div>Doo</div>
-        <div>Crystal Cove</div>
-      </div>
+      <br />
+      {tegelane !== "" && <div>Klikkisid tegelese {tegelane} peal</div>}
+      <br />
+      {tegelased.map((t, index) => (
+        <div>
+          <div key={index}>{t.firstName}</div>
+          <div>{t.lastName}</div>
+          <div>{t.location}</div>
+          <button onClick={() => kuvaNimi(t)}>Kuva nimi</button>
+        </div>
+      ))}
     </div>
   );
 }
